b9f770e2ca2bcd74e97e373e0181248671e7feb2
CrownKira <t.karwi@yahoo.com>
2021-05-27 01:57:46 +0800

    Fix wait for db

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         db_conn = None
         while not db_conn:
             try:
-                db_conn = connections["default"]
+                db_conn = connections["default"].cursor()
             except (OperationalError, Psycopyg2OpError):
                 self.stdout.write(
                     "Database unavailable, waiting for 1 second..."
