ad0768add785b8dbe9dee6388444b95c6947105f
CrownKira <t.karwi@yahoo.com>
2021-05-27 15:55:20 +0800

    Fix test commands

--- core/tests/test_commands.py
+++ core/tests/test_commands.py
@@ -5,11 +5,17 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 
 
+class MockConnection:
+    # TODO: review this
+    def cursor(*args, **kwargs):
+        return True
+
+
 class CommandTests(TestCase):
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
         with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
-            gi.return_value = True
+            gi.return_value = MockConnection()
             call_command("wait_for_db")
             self.assertEqual(gi.call_count, 1)
 
@@ -17,7 +23,7 @@ class CommandTests(TestCase):
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
         with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
-            gi.side_effect = [OperationalError] * 5 + [True]
+            gi.side_effect = [OperationalError] * 5 + [MockConnection()]
             call_command("wait_for_db")
 
             self.assertEqual(gi.call_count, 6)
