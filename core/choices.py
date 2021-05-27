136a35cb74928aaac4076022a47dfb96cfef72cb
CrownKira <t.karwi@yahoo.com>
2021-05-27 21:53:44 +0800

    Add user related models

---
+++ core/choices.py
@@ -0,0 +1,4 @@
+from django.utils.translation import gettext as _
+
+# https://stackoverflow.com/questions/24403075/django-choicefield
+GENDER_CHOICES = ((1, _("male")), (2, _("female")))
---
+++ core/models/__inti__.py
@@ -0,0 +1,2 @@
+# https://www.webforefront.com/django/modelsoutsidemodels.html
+# #:~:text=By%20default%2C%20Django%20models%20are,dozens%20or%20hundreds%20of%20models.
