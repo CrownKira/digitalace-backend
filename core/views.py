f6a99bd2c783579f625c1e2004a01a30de4f6abe
YuTao <tanyutao913@gmail.com>
2021-06-05 16:29:32 +0800

    Added multiple filter feature

--- core/views.py
+++ core/views.py
@@ -12,15 +12,15 @@ class BaseClassAttrForViewSet(viewsets.ModelViewSet):
 
     def _params_to_int(self, qs):
         """Convert string to list of integer"""
-        qs = qs[1:]
-        qs = qs[:-1]
         qs = qs.split(":")
         qsid = qs[0].strip('"')
         splited = qs[1]
         if "[" and "]" in splited:
+            splited = splited.split('],')[0]
             splited = re.sub(r"[\[\]]", "", splited)
             intsplit = [int(str_id) for str_id in splited.split(",")]
             return {qsid + "__in": intsplit}
+        splited = splited.strip(',')
         return {qsid: splited}
 
     def _param_to_str(self, qs):
@@ -31,14 +31,28 @@ class BaseClassAttrForViewSet(viewsets.ModelViewSet):
         qs = [i.strip('"') for i in qs]
         return qs
 
+    def _new_params_to_int(self, qs, queryset):
+        """Convert string to list of integer with respecive reference"""
+        qs = qs[1:]
+        qs = qs[:-1]
+        while qs.count('"') > 1:
+            splt_char = '"'
+            K = 3
+            temp = qs.split(splt_char)
+            res = splt_char.join(temp[:K]), splt_char.join(temp[K:])
+            qs = '"' + res[1]
+            fltr = res[0]
+            filtered = self._params_to_int(fltr)
+            queryset = queryset.filter(**filtered)
+        return queryset
+
     def get_queryset(self):
         """Custom queryset to support filtering sorting and listing"""
         queryset = self.queryset.order_by("id")
         filter = self.request.query_params.get("filter", None)
         sort = self.request.query_params.get("sort", None)
         if filter is not None:
-            filtered = self._params_to_int(filter)
-            queryset = queryset.filter(**filtered)
+            queryset = self._new_params_to_int(filter, queryset)
         if sort is not None:
             sorted = self._param_to_str(sort)
             if sorted[1] == "DESC":
