625a7a4829861d36153dd75821a93edf4348d8cb
CrownKira <t.karwi@yahoo.com>
2021-06-09 17:24:50 +0800

    Configure settings for production

--- api/settings/common.py
+++ api/settings/common.py
@@ -17,20 +17,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-z&mgjfk)n7eq!1e+g&l@jei1#yc5k2m6wo5s8=0ia2wd*n@b%="
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["localhost", "api"]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -88,10 +74,10 @@ WSGI_APPLICATION = "api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("DB_HOST"),
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("PGHOST"),
+        "NAME": os.environ.get("PGNAME"),
+        "USER": os.environ.get("PGUSER"),
+        "PASSWORD": os.environ.get("PGPASSWORD"),
     }
 }
 
