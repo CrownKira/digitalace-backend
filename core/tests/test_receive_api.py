86b234f41e206f854769a1b6db31d3c59e98cc49
YuTao <tanyutao913@gmail.com>
2021-06-13 11:08:49 +0800

    Rewrote sales_order, receive testcase

--- core/tests/test_receive_api.py
+++ core/tests/test_receive_api.py
@@ -13,6 +13,43 @@ from supplier.serializers import ReceiveSerializer
 RECEIVE_URL = reverse("supplier:receive-list")
 
 
+def create_purchase_order(**params):
+    return PurchaseOrder.objects.create(
+        date="2001-01-10",
+        payment_date="2001-01-10",
+        gst_rate="0.07",
+        discount_rate="0",
+        gst_amount="0",
+        discount_amount="0",
+        net="0",
+        total_amount="0",
+        grand_total="0",
+        status="PD",
+        **params
+        # supplier=supplier,
+        # company=self.company,
+    )
+
+
+def create_receive(**params):
+    return Receive.objects.create(
+        date="2001-01-10",
+        payment_date="2001-01-10",
+        gst_rate="0.07",
+        discount_rate="0",
+        gst_amount="0",
+        discount_amount="0",
+        net="0",
+        total_amount="0",
+        grand_total="0",
+        status="PD",
+        **params
+        # supplier=supplier,
+        # purchase_order=purchase_order,
+        # company=self.company,
+    )
+
+
 class PublicReceiveApiTest(TestCase):
     """Test the publicly available receive API"""
 
@@ -31,88 +68,70 @@ class PrivateReceiveApiTest(TestCase):
 
     def setUp(self):
         self.company = Company.objects.create(name="testcompany")
-        self.supplier = Supplier.objects.create(
-            company=self.company, name="testsupplier"
-        )
+        self.company2 = Company.objects.create(name="testcompany2")
         self.user = get_user_model().objects.create_user(
             "test@crownkiraappdev.com",
             "password123",
             is_staff=True,
             company=self.company,
         )
+        self.user2 = get_user_model().objects.create_user(
+            "test2@crownkiraappdev.com",
+            "password123",
+            is_staff=True,
+            company=self.company2,
+        )
+        self.supplier = Supplier.objects.create(
+            company=self.company, name="testsupplier"
+        )
+        self.supplier2 = Supplier.objects.create(
+            company=self.company2, name="testsupplier2"
+        )
         self.client = APIClient()
         self.client.force_authenticate(self.user)
 
     def test_retreive_receive(self):
         """Test retreiving receive"""
-        supplier = Supplier.objects.create(
-            company=self.company, name="testsupplier"
+        purchase_order = create_purchase_order(
+            **{"supplier": self.supplier, "company": self.company}
         )
-        purchase_order = PurchaseOrder.objects.create(
-            supplier=supplier,
-            company=self.company,
-            date="2001-01-10",
-            payment_date="2001-01-10",
-            gst_rate="0.07",
-            discount_rate="0",
-            gst_amount="0",
-            discount_amount="0",
-            net="0",
-            total_amount="0",
-            grand_total="0",
-            status="PD",
+        purchase_order2 = create_purchase_order(
+            **{"supplier": self.supplier, "company": self.company}
         )
-        purchase_order2 = PurchaseOrder.objects.create(
-            supplier=supplier,
-            company=self.company,
-            date="2001-01-10",
-            payment_date="2001-01-10",
-            gst_rate="0.07",
-            discount_rate="0",
-            gst_amount="0",
-            discount_amount="0",
-            net="0",
-            total_amount="0",
-            grand_total="0",
-            status="PD",
+        create_receive(
+            **{
+                "supplier": self.supplier,
+                "purchase_order": purchase_order,
+                "company": self.company,
+            }
         )
-        Receive.objects.create(
-            supplier=supplier,
-            purchase_order=purchase_order,
-            company=self.company,
-            date="2001-01-10",
-            payment_date="2001-01-10",
-            gst_rate="0.07",
-            discount_rate="0",
-            gst_amount="0",
-            discount_amount="0",
-            net="0",
-            total_amount="0",
-            grand_total="0",
-            status="PD",
+        create_receive(
+            **{
+                "supplier": self.supplier,
+                "purchase_order": purchase_order2,
+                "company": self.company,
+            }
         )
-        Receive.objects.create(
-            supplier=supplier,
-            purchase_order=purchase_order2,
-            company=self.company,
-            date="2001-01-10",
-            payment_date="2001-01-10",
-            gst_rate="0.07",
-            discount_rate="0",
-            gst_amount="0",
-            discount_amount="0",
-            net="0",
-            total_amount="0",
-            grand_total="0",
-            status="PD",
+        other_company_purchase_order = create_purchase_order(
+            **{"supplier": self.supplier2, "company": self.company2}
+        )
+        create_receive(
+            **{
+                "supplier": self.supplier2,
+                "purchase_order": other_company_purchase_order,
+                "company": self.company2,
+            }
         )
 
         res = self.client.get(RECEIVE_URL)
 
-        receives = Receive.objects.all().order_by("-id")
+        receives = (
+            Receive.objects.all().filter(company=self.company).order_by("-id")
+        )
         serializer = ReceiveSerializer(receives, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data.get("results", None), serializer.data)
+        self.assertEqual(len(res.data.get("results", [])), 2)
 
     # TODO: rewrite
     # def test_create_receive_successful(self):
