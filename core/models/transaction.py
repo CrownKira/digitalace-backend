5228c4fe874ac3d4717759c8f4c5f2227026bfa3
CrownKira <t.karwi@yahoo.com>
2021-07-14 22:11:19 +0800

    Make minor changes

--- core/models/transaction.py
+++ core/models/transaction.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models.deletion import SET_NULL
-from django.db.models.fields.related import ForeignKey
 from django.utils.translation import gettext_lazy as _
 
 
